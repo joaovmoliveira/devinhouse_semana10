7f3a59fa7064723ef81d7b54d51646b0a42721ce
João Victor Mendes de Oliveira <joao.m.oliveira@sesisc.org.br>
2021-02-11 20:56:24 -0300

    adicionados exemplos feitos na aula 4

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,126 +1,110 @@
 package teste;
 
 import java.util.Scanner;
+import java.util.Calendar;
 
 public class OlaMundo {
 
 	public static void main(String[] args) {
 
-		//String str1 = "João Victor";
-		//String str2 = "joão victor";
-
-		//boolean ehIgual = str1.equalsIgnoreCase(str2);
-		//char char0 = str1.charAt(6);
-		//int index = str1.indexOf('o');
-		//int lastIndex = str1.lastIndexOf('o');
+		Scanner input = new Scanner(System.in);
+		/*System.out.print("Adivinhe um número de 5 a 10: ");
+		int numeroDoUsuario = Integer.parseInt(input.nextLine());
 		
-		//String strCorrigida = str1.replace('a', 'ã');
+		int aleatorio = (int) (Math.random() * (10-5) + 5);
 		
-		//String substr = str1.substring(5, 8);
+		System.out.println("número gerado = " + aleatorio);
 		
-		//boolean vaziaOuEmBranco = str1.isBlank();
-		//boolean vazia = str1.isEmpty();
+		while (numeroDoUsuario != aleatorio) {
+			System.out.print("Errou! Tente novamente: ");
+			numeroDoUsuario = Integer.parseInt(input.nextLine());
+		}*/
 		
-		//int resto = 8 % 2;
-		
-		//double media = (8+7+8)/3;
-		
-		//int i = (3 + 4) * 2 / 2;
-		
-		//int idade = 18;
 		/*
-		 * idade++; idade -= 2; idade *= 2; idade++; idade /= 3; idade %= 2;
+		 * int numeroDoUsuario; int aleatorio = (int) (Math.random() * (10-5) + 5);
+		 * 
+		 * do { System.out.print("Adivinhe um número de 5 a 10: "); numeroDoUsuario =
+		 * Integer.parseInt(input.nextLine()); } while (numeroDoUsuario != aleatorio);
+		 * 
+		 * System.out.println("Parabéns! Você acertou!");
 		 */
-		//int id2 = ++idade;
 		
-		/*double resultPow = Math.pow(5, 2);
-		double resultSqrt = Math.sqrt(9);
-		double resultCbrt = Math.cbrt(27); 
-		double resultAbs = Math.abs(-10);
-		double floorRes = Math.floor(29.9);
-		double ceilRes = Math.ceil(30.1);
-		double roundRes = Math.round(30.1);
-		int rand = (int) (Math.random() * (100-10) + 10);*/
-		
-		/*String str = "10";
-		String strNew = new String("10");
-		
-		System.out.println(str);
-		System.out.println(strNew);
-		
-		boolean ehIgual = str.equals(strNew);*/
-		
-		boolean ehIgual = 10 == 10 ^ 9 > 10;
-		
-		int idade = 70;
-		
-		//String voto = (idade >= 16 && idade < 18) || (idade >= 70) ? "Opcional" : "Obrigatório";
-		
-		String voto;
-		if ((idade >= 16 && idade < 18) || (idade >= 70)) {
-			voto = "Opcional";
-		} else if (idade >= 18 && idade < 70) {
-			voto = "Obrigatório";
-		} else {
-			voto = "Proibido";
+		String nome = "João Victor";
+		for (int i = 0; i < nome.length(); i++) {
+			System.out.println(nome.charAt(i));
 		}
 		
 		
-		System.out.println(voto);
-		//System.out.println(idade);
+		
+		
+		/* Calendar cal = Calendar.getInstance();
+
+		int mesAtual = cal.get(Calendar.MONTH) + 1;
+		String strMes = "";
+
+		switch (mesAtual) {
+			case 1:
+				strMes = "Janeiro";
+				break;
+			case 2:
+				strMes = "Fevereiro";
+				break;
+			case 3:
+				strMes = "Março";
+				break;
+			case 4, 5, 6:
+				strMes = "Estamos no segundo trimestre do ano.";
+				break;
+			case 7, 8, 9, 10, 11, 12:
+				strMes = "Estamos no segundo semestre do ano.";
+				break;
+			default:
+				strMes = "Não foi possível identificar o mês corrente.";
+		}
+
+		System.out.println(strMes);*/
 
 		/*
-		 * String str = "asdasdassd";
+		 * final int MINHA_IDADE = 29;
 		 * 
-		 * int inteiro = Integer.parseInt(str);
+		 * MINHA_IDADE = 30;
+		 * 
+		 * System.out.println(MINHA_IDADE);
+		 */
+
+		// System.out.println(dataAtual);
+
+		// int anoCorrente = dataAtual.get(Calendar.YEAR);
+
+		// System.out.println(anoCorrente);
+
+		/*
+		 * Scanner entrada = new Scanner(System.in);
+		 * 
+		 * System.out.print("Digite seu ano de nascimento: ");
+		 * 
+		 * int anoNascimento = Integer.parseInt(entrada.nextLine());
+		 * 
+		 * int idade = anoCorrente - anoNascimento;
 		 */
 
 		/*
-		 * double altura = 1.75; String strAltura; int intAltura;
-		 */
-		// String nome = "João Victor";
-
-		// double media = (8+7+8)/3;
-
-		// int rand = (int) (Math.random() * (100-10) + 10);
-		// double dblRand = Math.random() * (100-10) + 10;
-		// int idade = 18;
-		// String status = (idade >= 18) ? "adulto" : "menor de idade";
-
-		// boolean ehPar = idade > 10 ^ idade == 18;
-
-		// System.out.println(ehPar);
-
-		/*
-		 * int lengthNome = nome.length(); String nomeCAPS = nome.toUpperCase(); String
-		 * nomeMinuscula = nome.toLowerCase(); String nomePodado =
-		 * "  João Victor  ".trim();
+		 * if (idade == 18) System.out.println("Você deve se alistar!"); else if (idade
+		 * > 18) System.out.println("Você já se alistou no passado."); else
+		 * System.out.println("Aguarde sua vez.");
 		 * 
-		 * System.out.println("tam: "+ lengthNome); System.out.println("caps: "+
-		 * nomeCAPS); System.out.println("minusc: "+ nomeMinuscula);
-		 * System.out.println("trim: "+ nomePodado);
+		 * System.out.println("Obrigado por usar nosso serviço.");
 		 */
 
 		/*
-		 * Scanner teclado = new Scanner(System.in);
+		 * String str1 = "DEVinHouse"; String str2 = "Vazia";
 		 * 
-		 * System.out.print("Digite sua altura: "); strAltura = teclado.nextLine();
+		 * if (str1 != null && !str1.isEmpty() && str1.length() > 8) { str2 =
+		 * str1.substring(1,8); }
 		 * 
-		 * altura = altura + Double.parseDouble(strAltura);
-		 * 
-		 * 
-		 * System.out.print("Sr. Usuário, digite seu nome: "); nome =
-		 * teclado.nextLine();
-		 * 
-		 * System.out.printf("%s, digite sua altura: ", nome); altura =
-		 * teclado.nextDouble();
-		 * 
-		 * System.out.format("%s, digite sua idade: ", nome); idade = teclado.nextInt();
-		 * 
-		 * System.out.
-		 * printf("O usuário chamado %s%nmede %.2fm de altura%ne tem %d anos de idade.",
-		 * nome, altura, idade);
+		 * System.out.println(str2);
 		 */
+
 	}
 
 }
